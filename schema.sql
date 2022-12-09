9511d1049fda882b03e507c27bb7d8673332b3d1
Joel-100 <joelkugonza@gmail.com>
2022-12-09 12:37:59 +0300

    create animals table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,12 @@
 /* Database schema to keep the structure of entire database. */
 
+-- Create animals table
 CREATE TABLE animals (
-    name varchar(100)
+    id int GENERATED ALWAYS AS IDENTITY,
+    name varchar(250),
+    date_of_birth date,
+	escape_attempts int,
+	neutered boolean,
+	weight_kg decimal,
+	PRIMARY KEY(id)
 );
